1c4110d34285b52715b5c82fe2472fc42d1f3f85
Gafar <104564468+iGafar@users.noreply.github.com>
2023-09-27 16:35:18 +0300

    made main page

--- js/main.js
+++ js/main.js
@@ -1,29 +1,44 @@
-const navList = document.getElementsByClassName(
+const navListIntelligence = document.getElementsByClassName(
   "header-center__nav-list-item"
 )[2];
-const intelligence = document.getElementsByClassName("header-center__nav__intelligence")[0]
+const navListServices = document.getElementsByClassName(
+  "header-center__nav-list-item"
+)[3];
+const intelligence = document.getElementsByClassName(
+  "header-center__nav__intelligence"
+)[0];
+const services = document.querySelector(".header-center__nav__services");
 
-navList.addEventListener("mouseover", function () {
-  intelligence.classList.add("header-center__nav__intelligence-active");
-});
+function addList(item, list) {
+  item.addEventListener("mouseover", function () {
+    list.classList.add("header-center__nav-active");
+  });
+}
 
-intelligence.addEventListener("mouseout", function () {
-  intelligence.classList.remove("header-center__nav__intelligence-active");
-});
+function removeList(list) {
+  list.addEventListener("mouseout", function () {
+    list.classList.remove("header-center__nav-active");
+  });
+  window.addEventListener("click", function () {
+    list.classList.remove("header-center__nav-active");
+  });
+}
 
-window.addEventListener("click", function() {
-  intelligence.classList.remove("header-center__nav__intelligence-active");
-})
+addList(navListIntelligence, intelligence);
+addList(navListServices, services);
+removeList(intelligence);
+removeList(services);
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   speed: 400,
   spaceBetween: 100,
+  loop: true,
   autoplay: {
     delay: 3000,
-    disableOnInteraction: false
+    disableOnInteraction: false,
   },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true
-  }
+    clickable: true,
+  },
 });
